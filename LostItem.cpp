a0c03dbe2c544531eed15dff1862b45c2a5deda2
anjananir1 <anjana.niranj@berkeley.edu>
2025-07-11 22:16:43 -0700

    Upload LostItem.cpp from X-Code

--- LostItem.cpp
+++ LostItem.cpp
@@ -1,1 +1,9 @@
+#include "LostItem.h"
+#include <iostream>
+using namespace std;
+
+// Show info for valuable items
+void ValuableItem::display() {
+    cout << name << " (Valuable), ID: " << id << endl;
+}
 
