40353f04712f32cbd4ac3f20fdddc75cfb9c563f
anjananir1 <anjana.niranj@berkeley.edu>
2025-07-11 22:18:03 -0700

    Upload LostItem.h from X-Code

--- LostItem.h
+++ LostItem.h
@@ -1,1 +1,38 @@
+#pragma once
+#include <string>
+#include <iostream>
+using namespace std;
+
+// Base class for all lost items
+class LostItem {
+protected:
+    string name;
+    int id;
+
+public:
+    LostItem(string n, int i) : name(n), id(i) {}
+    virtual void display() = 0; // must be implemented by child classes
+
+    int getId() { return id; }
+    string getName() { return name; }
+
+    virtual ~LostItem() {}
+};
+
+// Class for more valuable items like phones or wallets
+class ValuableItem : public LostItem {
+public:
+    ValuableItem(string n, int i) : LostItem(n, i) {}
+    void display() override; // defined in LostItem.cpp
+};
+
+// Class for basic items like bottles or notebooks
+class NonValuableItem : public LostItem {
+public:
+    NonValuableItem(string n, int i) : LostItem(n, i) {}
+
+    void display() override {
+        cout << name << " (Non-Valuable), ID: " << id << endl;
+    }
+};
 
